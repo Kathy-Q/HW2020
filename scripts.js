6acdbbf7b254d2f0fc52e24ce6440e22ca27eda9
Kathy <18778159487@163.com>
2020-06-15 13:37:05 +0800

    增加新内容

--- scripts.js
+++ scripts.js
@@ -2,6 +2,9 @@ let tasks = [];//{title:"ddddd", done:fasle}
 function renderEditor() {
     let inputE1 = document.querySelector("#default-todo-panel .todo-editor > input");
     let addTask = () => {
+        if (inputE1.value.length===0) {
+            return;
+        }
         let newTask = {
             title: inputE1.value,
             done: false,
@@ -39,23 +42,74 @@ function renderEditor() {
         for (let i = 0; i < tasks.length; i++) {
             let task = tasks[i];
             let itemE1 = document.createElement("div");
+            itemE1.className = "task";
+            
 
             let doneE1 = document.createElement("input");
             doneE1.type = "checkbox";
+            doneE1.checked = task.done;
+            if(task.done) {
+                itemE1.classList.add("done");
+        } else {
+            itemE1.classList.remove("done");
+        }
+            
+
+            doneE1.onchange = (e) => {
+               task.done = e.target.checked;
+               if(task.done) {
+                itemE1.classList.add("done");
+        } else {
+            itemE1.classList.remove("done");
+        }
+            }
             itemE1.append(doneE1);
 
             let titleE1 = document.createElement("label");
             titleE1.innerText = task.title;
             itemE1.append(titleE1);
 
-            let cancelE1 = document.createElement("button");
-            cancelE1.innerText = "X";
-            itemE1.append(cancelE1);
+            let ctrlbarE1 = renderTaskCtrlBar(tasks, i);
+            itemE1.append(ctrlbarE1);    
             itemsE1.append(itemE1);
 
 
 
         }
+    }
+
+    function renderTaskCtrlBar( tasks, taskIdx) {
+
+        let ctrlbarE1 = document.createElement("div");
+        ctrlbarE1.className = "ctrlbar";
+
+        let upE1 = document.createElement("button");
+        if (taskIdx === 0) {
+            upE1.disabled = true;
+
+        }
+        upE1.innerText = "↿";
+        upE1.onclick = () =>{
+        
+        };
+        ctrlbarE1.append(upE1); 
+
+        let downE1 = document.createElement("button");
+        downE1.innerText = "⇂";
+        downE1.onclick = () =>{
+
+        };
+        ctrlbarE1.append(downE1);
+
+        let cancelE1 = document.createElement("button");
+        cancelE1.innerText = "X";
+        cancelE1.onclick = () =>{
+            tasks.splice(taskIdx, 1);
+            renderTasksItems();
+        };
+        
+        ctrlbarE1.append(cancelE1); 
+        return ctrlbarE1;
     }
     renderEditor();
     renderTasksItems();
